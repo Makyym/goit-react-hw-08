0ab1c314fcbbf25bd1c52fbbba2c38be5268bdfa
Makyym <168359394+Makyym@users.noreply.github.com>
2025-03-02 14:13:04 +0200

    add register validation

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -4,10 +4,16 @@ import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useId } from "react";
+import * as Yup from "yup";
+import { ErrorMessage } from "formik";
 
 const RegistrationForm = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const nameFieldId = useId();
+    const phoneFieldId = useId();
+    const emailFieldId = useId();
     const handleSubmit = (values, options) => {
         dispatch(register(values))
             .unwrap()
@@ -30,16 +36,30 @@ const RegistrationForm = () => {
         "email": "",
         "password": ""
     };
+    const contactSchema = Yup.object().shape({
+        name: Yup.string().min(3, "Too short!").max(50, "Too long!").required("Required"),
+        email: Yup.string().email("Invalid email").required("Required"),
+        password: Yup.string().min(7, "Too short!").max(50, "Too long!").required("Required"),
+        });
     return (
-        <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={contactSchema}>
             <div className={s.relative}>
                 <Form className={s.form}>
-                    <div>
+                    <div className={s.div}>
                         <h2>Create an account to start using the application.</h2>
                         <h3>{`If you already have an account, click the 'Log In' link.`}</h3>
-                        <Field className={s.input} name='name' placeholder='Enter name' />
-                        <Field className={s.input} name='email' placeholder='Enter email' />
-                        <Field className={s.input} name='password' type='password' placeholder='Enter pass' />
+                        <div className={s.wrapper}>
+                            <Field className={s.input} name='name' placeholder='Enter name' id={nameFieldId}/>
+                            <ErrorMessage name="name" component="span"/>
+                        </div>
+                        <div className={s.wrapper}>
+                            <Field className={s.input} name='email' placeholder='Enter email' id={phoneFieldId}/>
+                            <ErrorMessage name="email" component="span"/>
+                        </div>
+                        <div className={s.wrapper}>
+                            <Field className={s.input} name='password' type='password' placeholder='Enter pass' id={emailFieldId}/>
+                            <ErrorMessage name="password" component="span"/>
+                        </div>
                         <button type="submit">Submit</button>
                     </div>
                 </Form>
